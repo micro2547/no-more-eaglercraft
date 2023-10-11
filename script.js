d8b9bef697208d4a636c22544c01e7d4e271f7ac
micro <113215676+micro2547@users.noreply.github.com>
2023-10-11 15:09:53 -0700

    Update script.js

--- script.js
+++ script.js
@@ -97,7 +97,7 @@ window.onload = async () => {
       video.play();
 
       const interval = setInterval(() => {
-        const time = video.currentTime - 4.3 - (step * 25) / 110; // 132 bpm moment
+        const time = video.currentTime - 4.3 - (step * 35) / 110; // 132 bpm moment
         if (step >= memes.length) step = -Infinity;
         if (step < 0) return clearInterval(interval);
         if (time >= 0) {
